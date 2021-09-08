1300f03e5c3c7eaf579a905f4920c05b4c973357
afeiship <1290657123@qq.com>
2021-09-08 20:18:00 +0800

    fix: optimze impl

--- public/src/app.tsx
+++ public/src/app.tsx
@@ -11,7 +11,13 @@ const Container = styled.div`
 export default (props: any) => {
   return (
     <Container>
-      <ReactEllipsisMiddle text="CSS 测试标题，这是一个稍微有点长的标题，超出一行以后才会有title提示，标题是 实现优惠券的技巧 - 2021-03-26" />
+      <ReactEllipsisMiddle size="120px">
+        CSS 测试标题，这是一个稍微有点长的标题，超出一行以后才会有title提示，标题是 实现优惠券的技巧 - 2021-03-26
+      </ReactEllipsisMiddle>
+
+      <ReactEllipsisMiddle size="50%">
+        CSS 测试标题，这是一个稍微有点长的标题，超出一行以后才会有title提示，标题是 实现优惠券的技巧 - 2021-03-26
+      </ReactEllipsisMiddle>
     </Container>
   );
 };
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,9 +2,10 @@ import classNames from 'classnames';
 import React, { Component } from 'react';
 import filterProps from '@jswork/filter-react-props';
 
-export type ReactEllipsisMiddleProps = { className?: string; text?: string; size?: number | string; };
-
+export type ReactEllipsisMiddleProps = { className?: string; children?: string; size?: number | string; };
 const CLASS_NAME = 'react-ellipsis-middle';
+
+// https://codepen.io/xboxyan/pen/eYvveBe
 
 export default class ReactEllipsisMiddle extends Component<ReactEllipsisMiddleProps> {
   static displayName = CLASS_NAME;
@@ -18,15 +19,15 @@ export default class ReactEllipsisMiddle extends Component<ReactEllipsisMiddlePr
   }
 
   render() {
-    const { className, text, size, ...props } = this.props;
+    const { className, children, size, ...props } = this.props;
     const theProps = filterProps(props);
 
     return (
       <div data-component={CLASS_NAME} className={classNames(CLASS_NAME, className)} {...theProps}>
-        <div className="is-fulltext">{text}</div>
-        <div className="is-half-text">
-          <div className="is-overlap" style={this.sizeStyle}>{text}</div>
-          {text}
+        <div className="is-fulltext">{children}</div>
+        <div className="is-halftext">
+          <div className="is-overlap" style={this.sizeStyle}>{children}</div>
+          {children}
         </div>
       </div>
     );
