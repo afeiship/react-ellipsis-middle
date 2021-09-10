67758cfd8c67b443e97a59c9d4c6ab8f4f68700e
afeiship <1290657123@qq.com>
2021-09-10 08:33:58 +0800

    feat: add minSize for ellipse

--- public/src/app.tsx
+++ public/src/app.tsx
@@ -4,19 +4,39 @@ import styled from 'styled-components';
 import '../../src/components/style.scss';
 
 const Container = styled.div`
-  width: 80%;
-  margin: 30px auto 0;
+  width: 600px;
+  margin: 50px auto;
+  border-radius: 8px;
+  padding: 15px;
+  overflow: hidden;
+  resize: horizontal;
+  box-shadow: 20px 20px 60px #bebebe, -20px -20px 60px #ffffff;
+
+  .react-ellipsis-middle {
+    cursor: pointer;
+    &:hover {
+      transition: all 0.5s ease;
+      background-color: #f60;
+    }
+  }
 `;
 
 export default (props: any) => {
   return (
     <Container>
       <ReactEllipsisMiddle size="120px">
-        CSS 测试标题，这是一个稍微有点长的标题，超出一行以后才会有title提示，标题是 实现优惠券的技巧 - 2021-03-26
+        CSS 测试标题，这是一个稍微有点长的标题，超出一行以后才会有title提示，标题是 实现优惠券的技巧
+        - 2021-03-26
       </ReactEllipsisMiddle>
 
       <ReactEllipsisMiddle size="50%">
-        CSS 测试标题，这是一个稍微有点长的标题，超出一行以后才会有title提示，标题是 实现优惠券的技巧 - 2021-03-26
+        CSS 测试标题，这是一个稍微有点长的标题，超出一行以后才会有title提示，标题是 实现优惠券的技巧
+        - 2021-03-26
+      </ReactEllipsisMiddle>
+
+      <ReactEllipsisMiddle>CSS 实现优惠券的技巧 - 2021-03-26</ReactEllipsisMiddle>
+      <ReactEllipsisMiddle minSize="40px">
+        我是一个很长的文件，我是一个很长的文件，但请始终保持我的后缀名.pdf
       </ReactEllipsisMiddle>
     </Container>
   );
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,20 +2,37 @@ import classNames from 'classnames';
 import React, { Component } from 'react';
 import filterProps from '@jswork/filter-react-props';
 
-export type ReactEllipsisMiddleProps = { className?: string; children?: string; size?: number | string; };
 const CLASS_NAME = 'react-ellipsis-middle';
+
+export type ReactEllipsisMiddleProps = {
+  /**
+   * The extended class name.
+   */
+  className?: string;
+  /**
+   * The text to display.
+   */
+  children?: string;
+  /**
+   * The width of ellipsis to display.
+   */
+  size?: number | string;
+  /**
+   * The min width of the text.
+   */
+  minSize: number | string;
+};
 
 // https://codepen.io/xboxyan/pen/eYvveBe
 
 export default class ReactEllipsisMiddle extends Component<ReactEllipsisMiddleProps> {
   static displayName = CLASS_NAME;
   static version = '__VERSION__';
-  static defaultProps = {
-    size: '50%',
-  };
+  static defaultProps = { size: '50%', minSize: 0 };
 
   get sizeStyle() {
-    return { width: this.props.size }
+    const { size, minSize } = this.props;
+    return { width: size, minWidth: minSize };
   }
 
   render() {
@@ -26,7 +43,9 @@ export default class ReactEllipsisMiddle extends Component<ReactEllipsisMiddlePr
       <div data-component={CLASS_NAME} className={classNames(CLASS_NAME, className)} {...theProps}>
         <div className="is-fulltext">{children}</div>
         <div className="is-halftext">
-          <div className="is-overlap" style={this.sizeStyle}>{children}</div>
+          <div className="is-overlap" style={this.sizeStyle}>
+            {children}
+          </div>
           {children}
         </div>
       </div>
